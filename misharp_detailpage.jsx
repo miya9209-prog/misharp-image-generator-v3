5e84ac2ec26ea113994e7fb02d27f6a6e147a5f3
miya9209-prog <miya9209@gmail.com>
2026-02-08 01:52:11 +0900

    Update misharp_detailpage.jsx

--- misharp_detailpage.jsx
+++ misharp_detailpage.jsx
@@ -3,13 +3,12 @@ app.displayDialogs = DialogModes.NO;
 
 (function () {
 
-    // ====== 미샵 기본값(고정) ======
+    // ===== MISHARP 고정 레이아웃 =====
     var CANVAS_WIDTH = 900;
     var TOP_MARGIN = 80;
     var GAP = 70;
     var BOTTOM_MARGIN = 120;
 
-    var FOOTER_ENABLED = true;
     var FOOTER_TEXT = "© MISHARP. All rights reserved.  |  misharp.co.kr";
     var FOOTER_FONT = "MalgunGothic";
     var FOOTER_SIZE = 18;
@@ -18,9 +17,9 @@ app.displayDialogs = DialogModes.NO;
 
     function px(v) { return new UnitValue(v, "px"); }
 
-    function getImageFiles(folder) {
+    function getImages(folder) {
         var files = folder.getFiles(function (f) {
-            return (f instanceof File) && f.name.match(/\.(jpg|jpeg|png)$/i) && f.name.indexOf("._") !== 0;
+            return f instanceof File && f.name.match(/\.(jpg|jpeg|png)$/i);
         });
         files.sort(function (a, b) { return a.name > b.name ? 1 : -1; });
         return files;
@@ -34,8 +33,7 @@ app.displayDialogs = DialogModes.NO;
         return { w: w, h: h };
     }
 
-    function placeSmartObject(file) {
-        // Place Embedded => Smart Object 레이어
+    function placeSO(file) {
         var desc = new ActionDescriptor();
         desc.putPath(charIDToTypeID("null"), file);
         desc.putEnumerated(charIDToTypeID("FTcs"), charIDToTypeID("QCSt"), charIDToTypeID("Qcsa"));
@@ -43,11 +41,10 @@ app.displayDialogs = DialogModes.NO;
         return app.activeDocument.activeLayer;
     }
 
-    function resizeToWidth(layer, targetW) {
+    function resizeToWidth(layer) {
         var b = layer.bounds;
         var w = b[2].as("px") - b[0].as("px");
-        if (w <= 0.01) return;
-        var scale = (targetW / w) * 100;
+        var scale = (CANVAS_WIDTH / w) * 100;
         layer.resize(scale, scale, AnchorPosition.TOPLEFT);
     }
 
@@ -56,54 +53,30 @@ app.displayDialogs = DialogModes.NO;
         layer.translate(x - b[0].as("px"), y - b[1].as("px"));
     }
 
-    function addFooter(doc, y) {
-        if (!FOOTER_ENABLED) return;
+    // ===== 실행 =====
+    var folder = Folder.selectDialog("상세페이지에 사용할 이미지 폴더를 선택하세요");
+    if (!folder) return;
 
-        var textLayer = doc.artLayers.add();
-        textLayer.kind = LayerKind.TEXT;
-        textLayer.name = "copyright";
-
-        var ti = textLayer.textItem;
-        ti.contents = FOOTER_TEXT;
-        ti.size = FOOTER_SIZE;
-        try { ti.font = FOOTER_FONT; } catch (e) {}
-
-        ti.color.rgb.red = FOOTER_COLOR[0];
-        ti.color.rgb.green = FOOTER_COLOR[1];
-        ti.color.rgb.blue = FOOTER_COLOR[2];
-
-        ti.justification = Justification.CENTER;
-        ti.position = [px(CANVAS_WIDTH / 2), px(y + FOOTER_SIZE * 2)];
-    }
-
-    // ====== 여기서부터 “그때 방식” ======
-    // JSX 실행하면 이미지 폴더만 선택 -> 바로 PSD 생성
-    var imgFolder = Folder.selectDialog("images 폴더를 선택하세요 (JPG/PNG가 들어있는 폴더)");
-    if (!imgFolder) return;
-
-    var images = getImageFiles(imgFolder);
+    var images = getImages(folder);
     if (images.length === 0) {
-        alert("선택한 폴더에 이미지(JPG/PNG)가 없습니다.");
+        alert("선택한 폴더에 이미지가 없습니다.");
         return;
     }
 
-    // 전체 높이 계산
-    var totalImagesH = 0;
-    var scaledHeights = [];
+    var heights = [];
+    var totalH = TOP_MARGIN + BOTTOM_MARGIN + FOOTER_MARGIN_TOP + FOOTER_SIZE * 2;
+
     for (var i = 0; i < images.length; i++) {
         var s = getSize(images[i]);
         var h = Math.round(s.h * (CANVAS_WIDTH / s.w));
-        scaledHeights.push(h);
-        totalImagesH += h;
+        heights.push(h);
+        totalH += h;
+        if (i < images.length - 1) totalH += GAP;
     }
 
-    var footerH = FOOTER_ENABLED ? (FOOTER_MARGIN_TOP + Math.round(FOOTER_SIZE * 2.2) + 12) : 0;
-    var canvasH = TOP_MARGIN + totalImagesH + GAP * (images.length - 1) + BOTTOM_MARGIN + footerH;
-
-    // PSD 생성
     var doc = app.documents.add(
         px(CANVAS_WIDTH),
-        px(canvasH),
+        px(totalH),
         72,
         "misharp_detailpage",
         NewDocumentMode.RGB,
@@ -112,24 +85,32 @@ app.displayDialogs = DialogModes.NO;
 
     var y = TOP_MARGIN;
 
-    // 이미지 배치(모두 Smart Object)
     for (var j = 0; j < images.length; j++) {
-        placeSmartObject(images[j]);
+        placeSO(images[j]);
         var layer = doc.activeLayer;
         layer.name = images[j].name;
 
-        resizeToWidth(layer, CANVAS_WIDTH);
+        resizeToWidth(layer);
         moveLayer(layer, 0, y);
 
-        y += scaledHeights[j] + GAP;
+        y += heights[j] + GAP;
     }
 
-    // 카피라이트
-    if (FOOTER_ENABLED) {
-        y += FOOTER_MARGIN_TOP;
-        addFooter(doc, y);
-    }
+    // footer
+    y += FOOTER_MARGIN_TOP;
+    var t = doc.artLayers.add();
+    t.kind = LayerKind.TEXT;
+    t.name = "copyright";
+    var ti = t.textItem;
+    ti.contents = FOOTER_TEXT;
+    ti.size = FOOTER_SIZE;
+    try { ti.font = FOOTER_FONT; } catch (e) {}
+    ti.color.rgb.red = FOOTER_COLOR[0];
+    ti.color.rgb.green = FOOTER_COLOR[1];
+    ti.color.rgb.blue = FOOTER_COLOR[2];
+    ti.justification = Justification.CENTER;
+    ti.position = [px(CANVAS_WIDTH / 2), px(y + FOOTER_SIZE * 2)];
 
-    alert("완료!\nSmart Object 레이어가 살아있는 PSD가 생성되었습니다.");
+    alert("완료! Smart Object PSD가 생성되었습니다.");
 
 })();
